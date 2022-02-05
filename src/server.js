f2c7e1f7af8c801977a1aca36cd8ab9109fe0952
abdinazarovDV <95746506+abdinazarovDV@users.noreply.github.com>
2022-02-05 11:42:34 +0500

    cors added

--- src/server.js
+++ src/server.js
@@ -9,7 +9,9 @@ const app = express();
 app.use( express.json() );
 app.use( fileUpload() );
 app.use( express.json() );
-app.use( cors() );
+app.use( cors({
+    "origin": "https://pressa-n2.netlify.app"
+}) );
 
 import { jsonHelper } from "#mid/jsonhelp";
 import verify from '#mid/verify';
